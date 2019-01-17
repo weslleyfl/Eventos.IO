8c0c07ac696b9ee9c79c6330236ec4620bd3739d
oweslley <oweslley@hotmail.com>
2019-01-17 20:27:11 -0200

    Segundo dia de desenvolvimento criaçao do projeto Domain core base

---
+++ src/Eventos.IO.Domain.Core/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Core.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+    }
+}
