2fdc28da90b59d0f87325f0deebb95db4b81ca22
oweslley <oweslley@hotmail.com>
2019-01-23 19:42:06 -0200

    6 Parte inicio da construçao do IUnitOfWork

---
+++ src/Eventos.IO.Domain/CommandHandlers/CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using System.Text;
+using Eventos.IO.Domain.Interfaces;
+
+namespace Eventos.IO.Domain.CommandHandlers
+{
+    public abstract class CommandHandler
+    {
+        private readonly IUnitOfWork _uow;
+
+        protected CommandHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        protected void NotificarValidacoesErro(ValidationResult validationResult)
+        {
+            foreach (var erro in validationResult.Errors)
+            {
+                Console.WriteLine(erro.ErrorMessage);
+            }
+
+        }
+
+        protected bool Commit()
+        {
+            // TODO: Validar antes do commit se há alguma validaçao de negocio com erro!
+
+            var responseCommand = _uow.Commit();
+            if (responseCommand.Success) return true;
+
+            Console.WriteLine("Ocorreu um erro ao salvar os dados no banco.");
+            return false;
+
+        }
+    }
+}
---
+++ src/Eventos.IO.Domain/EventosRoot/CommandsHandlers/EventoCommandHandlers.cs
@@ -0,0 +1,51 @@
+﻿using Eventos.IO.Domain.CommandHandlers;
+using Eventos.IO.Domain.Core.Events;
+using Eventos.IO.Domain.EventosRoot.Commands;
+using Eventos.IO.Domain.EventosRoot.Repository;
+using Eventos.IO.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.EventosRoot.CommandsHandlers
+{
+    public class EventoCommandHandlers : CommandHandler, IHandler<RegistrarEventoCommand>, IHandler<AtualizarEventoCommand>, IHandler<ExcluirEventoCommand>
+    {
+        // Injeçao de dependencia
+        private readonly IEventoRespository _eventoRespository;
+
+        public EventoCommandHandlers(IEventoRespository eventoRespository, IUnitOfWork uow) : base(uow)
+        {
+            _eventoRespository = eventoRespository;
+        }
+
+        public void Handle(RegistrarEventoCommand message)
+        {
+            var evento = new Evento(nome: message.Nome, dateInicio: message.DataInicio, dataFim: message.DataFim, gratuito: message.Gratuito, valor: message.Valor, online: message.Online, nomeEmpresa: message.NomeDaEmpresa);
+
+            if (!evento.EhValido())
+            {
+                NotificarValidacoesErro(evento.ValidationResult);
+                return;
+            }
+
+            // TODO: Validaçao do negocio no command
+            // Pode tratar regras de negocio aqui tambem, se nao for o caso de tratar lá na entidade - Evento
+            // Validaçoes de Negocio Exemplo
+            // O Organizador pode registrar um evento? (tipo sera que ele pagou a taxa de abertura)
+
+            // Persistencia
+            _eventoRespository.Add(evento);
+        }
+
+        public void Handle(AtualizarEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ExcluirEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
